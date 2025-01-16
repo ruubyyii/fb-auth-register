2859017cd5504ead3a0595dfdb625808132e06b4
Aaron Campos <aaroncm611@gmail.com>
2025-01-16 09:43:34 +0100

    Empenzando con Firebase

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,14 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'RegisterView',
+    component: () => import('@/views/RegisterView.vue')
   },
   {
-    path: '/about',
+    path: '/LoginView',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
