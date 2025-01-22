6b5ba318fe4aeffa1609cf422bbdf10b07ac0845
rubyyii <rubyyii65@gmail.com>
2025-01-22 20:54:42 +0100

    Pongo estilos a la pagina dandola por terminada.

--- src/views/PerfilView.vue
+++ src/views/PerfilView.vue
@@ -1,7 +1,9 @@
 <template>
-    <section>
-        <h1>Bienvenido {{  user.email }}</h1>
-        <button @click="logout">Cerrar sesion</button>
+    <section class="flex flex-col justify-center items-center h-screen">
+        <div class="flex flex-col justify-center items-center gap-10 bg-[whitesmoke] p-20 rounded-md">
+            <h1 class="text-3xl">Bienvenido {{  user.email }}</h1>
+        </div>
+        <button class=" hover:scale-105 transition p-2 mt-4 bg-red-600 shadow-lg text-white rounded-md font-bold" @click="logout">Cerrar sesion</button>
     </section>
 </template>
 
@@ -34,6 +36,11 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+section{
+    background: #000000;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to bottom, #070606, #000000);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to bottom, #434343, #000000); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
+}
 </style>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,18 +1,20 @@
 <template>
-    <section>
-        <h2 class=" text-red-600 text-xl">Register</h2>
-        <form @submit.prevent="registerUser">
-            <div>
-                <label for="email">Correo Electrónico</label>
-                <input type="email" id="email" v-model="email" required placeholder="Correo Electrónico">
-            </div>
-            <div>
-                <label for="password">Contraseña</label>
-                <input type="password" id="password" v-model="password" required placeholder="Contraseña">
-            </div>
-            <button type="submit">Registrarse</button>
-        </form>
-        <p>Ya tienes cuenta? <router-link to="/login">Inicia Sesion</router-link></p>
+    <section class="flex flex-col justify-center items-center h-screen gap-8 shadow-inner">
+        <div id="cont_form" class="flex flex-col justify-center items-center gap-10 bg-[whitesmoke] p-20 rounded-md">
+            <h2 class="text-4xl font-bold tracking-wider drop-shadow-md">REGISTER</h2>
+            <form @submit.prevent="registerUser" class="flex flex-col gap-5">
+                <div class="flex">
+                    <label class="shadow-md p-2 px-3 bg-green-500 text-white" for="email"><i class="fa-solid fa-envelope"></i></label>
+                    <input class="shadow-md outline-none rounded-r-md ps-2" type="email" id="email" v-model="email" required placeholder="Correo Electrónico">
+                </div>
+                <div class="flex">
+                    <label class="shadow-md p-2 px-3 bg-green-500 text-white" for="password"><i class="fa-solid fa-lock"></i></label>
+                    <input class="shadow-md outline-none rounded-r-md ps-2" type="password" id="password" v-model="password" required placeholder="Contraseña">
+                </div>
+                <button type="submit" class=" hover:scale-105 transition p-2 mt-4 bg-green-600 shadow-lg text-white rounded-md font-bold">Registrarse</button>
+            </form>
+            <p>Ya tienes cuenta? <router-link to="/login" class="text-green-700 underline">Inicia Sesion</router-link></p>
+        </div>
     </section>
 </template>
 
@@ -41,3 +43,10 @@ export default {
     }
 }
 </script>
+
+<style lang="sass" scoped>
+section
+    background: #52c234  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to top, #061700, #52c234)  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to top, #061700, #52c234) /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+</style>
